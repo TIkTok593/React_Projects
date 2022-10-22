e58a2ae405740ffdff72d9791f554841b2f6f5dd
abuyahyadiab <abuyahyadiab@abuyahyadiabs-MacBook-Pro.local>
2022-10-22 16:27:15 +0300

    First step with component functions

---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+function Nav() {
+    return (
+        <nav class="main-nav">
+            <ul>
+                <li>Home</li>
+                <li>Articles</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    )
+};
+
+export default Nav;
